c0466c94d5ba557c1b35526c76a4814c733f924e
Daniel anthony Dela cruz <anthonydelac1602@gmail.com>
2025-05-04 18:40:58 +0800

    mod loop in main c

--- main.c
+++ main.c
@@ -2,7 +2,13 @@
 
 int main(){
 
-   printf("Hello world");
+     int n;
+    
+    for(n=0; n <=10; n++){
+          printf("%d",n);
+          printf(". hello world\n");
+          
+    }
 
   return 0;
 }
